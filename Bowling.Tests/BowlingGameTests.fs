173fd0c93f98b193205c1091407c8d207744fd67
lmcauley <lmcauley@winnipeg.ca>
2014-11-26 13:53:39 -0600

    Initial Commit

---
+++ Bowling.Tests/BowlingGameTests.fs
@@ -0,0 +1,17 @@
+﻿namespace Bowling.Tests
+
+open NUnit.Framework
+open FsUnit
+open System
+open Bowling
+
+module ``Roll Method`` =
+
+  //Test Gutter Game
+  type ``When knocking down zero pins``() =
+        
+    [<Test>]
+    member this.``Score Should Be Zero`` () =
+        let actual = Game.roll 0
+
+        actual |> should equal (0)
---
+++ Bowling/Game.fs
@@ -0,0 +1,8 @@
+﻿namespace Bowling
+
+module Game =
+    let roll pins = 
+        0
+    
+    let score () = 
+        0 
